9e3397f8165289640c01d2d323562938c4cb522e
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2015-09-08 17:08:40 -0600

    Hooking up sounds

--- app.js
+++ app.js
@@ -5,7 +5,11 @@ var serialPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 9600
 });
 
-new Sound('/home/pi/pibot/sounds/wakey/wakey0.wav').play();
+var randomAudio = function (num, folder) {
+  var n = Math.floor(Math.random() * (num + 1));
+  var s = '/home/pi/pibot/sounds/' + folder + '/' + folder + n.toString() + '.wav'
+  return s;
+};
 
 serialPort.on('open', function () {
   console.log('open');
@@ -13,35 +17,42 @@ serialPort.on('open', function () {
     console.log('data received: ' + data);
     if (data == 2) {
       console.log('Hello');
-      new Sound('/sounds/wakey/wakey0.wav').play();
+      new Sound(randomAudio(1, 'wakey')).play();
     }
 
     if (data == 3) {
       console.log('Searching...');
+      new Sound(randomAudio(6, 'searching')).play();
     }
 
     if (data == 4) {
       console.log('Stop searching...');
+      new Sound(randomAudio(2, 'stop')).play();
     }
 
     if (data == 5) {
       console.log('Found.');
+      new Sound(randomAudio(4, 'found')).play();
     }
 
     if (data == 6) {
       console.log('Lost.');
+      new Sound(randomAudio(3, 'lost')).play();
     }
 
     if (data == 7) {
       console.log('Gotcha.');
+      new Sound(randomAudio(3, 'gotcha')).play();
     }
 
     if (data == 8) {
       console.log('Wakey.');
+      new Sound(randomAudio(2, 'wakey')).play();
     }
 
     if (data == 9) {
       console.log('Sleep.');
+      new Sound(randomAudio(4, 'sleep')).play();
     }
 
 
