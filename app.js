d3152df99354468f24466c5fa91af7ee2afcc78d
Cory Grunkemeyer <corygrunk@gmail.com>
2015-09-09 05:24:14 +0000

    working on speech

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 var SerialPort = require('serialport').SerialPort;
 //var Sound = require('node-aplay');
-var say = require('say')
+//var say = require('say')
+var pico = require('picotts');
+
+pico.say('Hey this is cool', 'en-US', function(err) {
+  if (!err)
+      console.log('Correctly played')
+});
 
 var serialPort = new SerialPort("/dev/ttyACM0", {
   baudrate: 9600
@@ -18,50 +24,50 @@ serialPort.on('open', function () {
     console.log('data received: ' + data);
     if (data == 2) {
       console.log('Hello');
-      say.speak(null, 'Hello');
-      // new Sound(randomAudio(1, 'wakey')).play();
+      //say.speak(null, 'Hello');
+      //new Sound(randomAudio(1, 'wakey')).play();
     }
 
     if (data == 3) {
       console.log('Searching...');
-      say.speak(null, 'Searching...');
-      // new Sound(randomAudio(6, 'searching')).play();
+      //say.speak(null, 'Searching');
+      //new Sound(randomAudio(6, 'searching')).play();
     }
 
     if (data == 4) {
       console.log('Stop searching...');
-      say.speak(null, 'Stop searching...');
-      // new Sound(randomAudio(2, 'stop')).play();
+      //say.speak(null, 'Stop searching');
+      //new Sound(randomAudio(2, 'stop')).play();
     }
 
     if (data == 5) {
       console.log('Found.');
-      say.speak(null, 'Found.');
-      // new Sound(randomAudio(4, 'found')).play();
+      //say.speak(null, 'Found');
+      //new Sound(randomAudio(4, 'found')).play();
     }
 
     if (data == 6) {
       console.log('Lost.');
-      say.speak(null, 'Lost.');
-      // new Sound(randomAudio(3, 'lost')).play();
+      //say.speak(null, 'Lost');
+      //new Sound(randomAudio(3, 'lost')).play();
     }
 
     if (data == 7) {
       console.log('Gotcha.');
-      say.speak(null, 'Gotcha.');
-      // new Sound(randomAudio(3, 'gotcha')).play();
+      //say.speak(null, 'Gotcha');
+      //new Sound(randomAudio(3, 'gotcha')).play();
     }
 
     if (data == 8) {
       console.log('Wakey.');
-      say.speak(null, 'Wakey.');
-      // new Sound(randomAudio(2, 'wakey')).play();
+      //say.speak(null, 'Wakey');
+      //new Sound(randomAudio(2, 'wakey')).play();
     }
 
     if (data == 9) {
       console.log('Sleep.');
-      say.speak(null, 'Sleep.');
-      // new Sound(randomAudio(4, 'sleep')).play();
+      //say.speak(null, 'Sleep');
+      //new Sound(randomAudio(4, 'sleep')).play();
     }
 
 
