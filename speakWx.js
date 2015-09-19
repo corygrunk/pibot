03950baef5cbf329d7e838b11dd5b8895c62ddbe
Cory Grunkemeyer <corygrunk@gmail.com>
2015-09-19 20:25:28 +0000

    WIP

--- speakWx.js
+++ speakWx.js
@@ -3,7 +3,7 @@ var pico = require('picotts');
 var wxEndpoint = "https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22denver%2C%20co%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys"
 
 pico.say("Ok, checking the forecast.", 'en-US', function(err) {
-  if (!err) { console.log(say);  }
+  if (!err) { console.log("Ok, checking the forecast.");  }
 });
 
 request(wxEndpoint, function (error, response, body) {
