acee774d3f6e3db84ef1892dd3ab727ae5131b53
Cory Grunkemeyer <corygrunk@gmail.com>
2015-09-23 18:13:55 +0000

    test-leds WIP

--- test-led.js
+++ test-led.js
@@ -1,7 +1,11 @@
-var leds = require('./lib/led.js');
+var leds = require('./lib/leds.js');
 
 var leds = new leds();
 
 leds.on('red', function () {
 	console.log('Red LED on.');
+})
+
+leds.off(function () {
+	console.log('Lights out.');
 })
