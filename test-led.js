d48dbe5cdc023f1da2625517fe47f950ea5c0eb3
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2015-09-23 12:16:56 -0600

    Leds module WIP

--- test-led.js
+++ test-led.js
@@ -1,11 +1,6 @@
 var leds = require('./lib/leds.js');
-
 var leds = new leds();
 
-leds.on('red', function () {
-	console.log('Red LED on.');
-})
+leds.on('red');
 
-leds.off(function () {
-	console.log('Lights out.');
-})
+leds.off();
