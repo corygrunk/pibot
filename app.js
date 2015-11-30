4564d5dc64b5e0928ed18df441d0152eb626c161
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2015-11-30 14:53:01 -0700

    Adding GPIO and LEDS

--- app.js
+++ app.js
@@ -4,14 +4,40 @@ var sp = new SerialPort("/dev/ttyUSB0", {
   baudrate: 19200,
   parser: serialport.parsers.readline("\n")
 });
+var sys = require('sys');
+var exec = require('child_process').exec;
+var child;
+var Gpio = require('onoff').Gpio;
+
+// GPIO PINS
+var ledRed = new Gpio(15, 'out');
+var ledGreen = new Gpio(24, 'out');
+var ledBlue = new Gpio(25, 'out');
 
 // SENSOR OBJECT - senses.distance & senses.motion
 var senses = {};
 
+// LED FUNCTIONS
+var ledOn = function (color) {
+  if (color === "red") {
+    ledRed.writeSync(1); ledGreen.writeSync(0); ledBlue.writeSync(0);
+  } else if (color === "green") {
+    ledRed.writeSync(0); ledGreen.writeSync(1); ledBlue.writeSync(0);
+  } else if (color === "blue") {
+    ledRed.writeSync(0); ledGreen.writeSync(0); ledBlue.writeSync(1);
+  } else {
+    ledRed.writeSync(0); ledGreen.writeSync(0); ledBlue.writeSync(1);
+  }
+}
+var lightsOut = function () {
+  ledRed.writeSync(0); ledGreen.writeSync(0); ledBlue.writeSync(0); 
+}
+
 // INIT
 console.log("Starting up...");
-
-
+ledBlue.writeSync(0);
+ledRed.writeSync(0); 
+ledGreen.writeSync(0); 
 
 // TURN ON ARDUINO SERIAL COMMUNITCATION
 sp.on('open', function () {
@@ -19,7 +45,21 @@ sp.on('open', function () {
   sp.on('data', function(data) {
     if (data.charAt(0) === "{" && data.charAt(data.length - 1) === "}") {
       senses = JSON.parse(data);
-    }
+    };
     console.log(senses);
+    if (senses.distance < 20) {
+    	ledOn(blue);
+    } else {
+    	lightsOut();
+    };
   });
 });
+
+// EXIT
+var exit = function () {
+  ledBlue.writeSync(0);
+  ledRed.writeSync(0);
+  ledGreen.writeSync(0);
+  process.exit();
+}
+process.on('SIGINT', exit);
