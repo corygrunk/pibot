77df9fe3c6847389d32c7a2681da84db5e3eb51f
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2016-01-19 12:02:56 -0700

    Adding some debug stuff

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ sp.on('open', function () {
 var waiting = 0;
 var searching = 0;
 var locked = 0;
+var logState = ' /  waiting: ' + waiting + ' / searching: ' + ' / locked: ' + locked + ' / motion: ' + senses.motion + ' / distance: ' + senses.motion;
 
 var reset = function () {
   waiting = 0;
@@ -47,7 +48,7 @@ var states = function () {
     }
     // MOTION DETECTED
     if (senses.motion === 1 && waiting === 0 && locked === 0) {
-      console.log(senses.motion + ' //////////////////////////////// Is someone there? ' + waiting);
+      console.log('//////////////////////////////// Is someone there?' + logState);
       waiting = 1;
     }
     if (waiting === 1) {
@@ -56,11 +57,11 @@ var states = function () {
     // SEARCHING...
     if (waiting >= 2 && waiting <= 10) {
       if (senses.distance > 10 && senses.distance < 30) {
-        console.log(senses.motion + ' //////////////////////////////// Locking ... locked: ' + locked + ' / distance: ' + senses.distance);
+        console.log('//////////////////////////////// Locking ...' + logState);
         locked = locked + 1;
         waiting = 2;
       } else {
-        console.log(senses.motion + ' //////////////////////////////// Searching... ' + waiting + ' / distance: ' + senses.distance);
+        console.log('//////////////////////////////// Searching...' + logState);
         waiting = waiting + 1;
       }
     }
