3157b67964a1ea889d60a9f84c04a2c8d30679c7
Cory Grunkemeyer <cory.grunkemeyer@insidetrack.com>
2016-01-29 16:16:08 -0700

    Trying to fix radio playing over record

--- intents/lowConfidence.js
+++ intents/lowConfidence.js
@@ -7,7 +7,9 @@ var sounds = [
 var randomSound = sounds[Math.floor(Math.random() * sounds.length)];
 
 var intent = function () {
-	sox.play(randomSound);
+	sox.play(randomSound, function () {
+		console.log(randomSound);
+	});
 }
 
 module.exports.intent = intent;
